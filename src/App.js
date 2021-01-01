eab63848e404c766e121eb0deeeae0974b801c8a
Mateusz Grzejszczyk <mateuszgrzejszczyk1@gmail.com>
2021-01-01 16:14:31 +0100

    adding reload

--- src/App.js
+++ src/App.js
@@ -26,33 +26,41 @@ const carArray = [
 const sortedCarArray = carArray.sort(() => Math.random() - 0.5);
 const carId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 class App extends React.Component {
+  handleReload() {
+    window.location.reload(true);
+  }
   render() {
-    const { startClicked,start,pairedArray,startTimeStamp} = this.props;
+    const { startClicked, start, pairedArray, startTimeStamp } = this.props;
     return (
       <>
-        <button onClick={startClicked} className="button">
-          {
-            start?"Good Luck":"START"
-          }
+        <button
+          onClick={() => {
+            pairedArray.length !== 6 && startClicked();
+            pairedArray.length === 6 && this.handleReload();
+          }}
+          className="button"
+        >
+          {start ? pairedArray.length === 6 ?"AGAIN ?": "Good Luck" : "START"}
         </button>
         <div id="App">
-          {sortedCarArray
-            .map((carBrand, i) => (
-              <Car carBrand={carBrand} carId={carId[i]} key={carId[i]} />
-            ))}
+          {sortedCarArray.map((carBrand, i) => (
+            <Car carBrand={carBrand} carId={carId[i]} key={carId[i]} />
+          ))}
         </div>
-        {
-          pairedArray.length===6&&<div className="grats">SUPER ! YOUR TIME: {(Date.now() - startTimeStamp)/1000} s.</div>
-        }
+        {pairedArray.length === 6 && (
+          <div className="grats">
+            SUPER ! YOUR TIME: {(Date.now() - startTimeStamp) / 1000} s.
+          </div>
+        )}
       </>
     );
   }
-};
-
-const mapStateToProps = (state)=>{
-  const {start,pairedArray, startTimeStamp}=state;
-  return {start,pairedArray,startTimeStamp};
 }
+
+const mapStateToProps = (state) => {
+  const { start, pairedArray, startTimeStamp } = state;
+  return { start, pairedArray, startTimeStamp };
+};
 
 const mapDispatchToProps = (dispatch) => ({
   startClicked: () => dispatch(startClicked()),
